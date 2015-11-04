30e896ea10c7e536dd176fb580effa9d2627528a
Martin Samson <martin.samson@shopify.com>
2015-11-04 09:40:43 -0500

    Update __init__.py

---
+++ tc_mongodb/__init__.py
@@ -0,0 +1,5 @@
+
+Config.define('MONGO_STORAGE_SERVER_HOST', 'localhost', 'MongoDB storage server host', 'MongoDB Storage')
+Config.define('MONGO_STORAGE_SERVER_PORT', 27017, 'MongoDB storage server port', 'MongoDB Storage')
+Config.define('MONGO_STORAGE_SERVER_DB', 'thumbor', 'MongoDB storage server database name', 'MongoDB Storage')
+Config.define('MONGO_STORAGE_SERVER_COLLECTION', 'images', 'MongoDB storage image collection', 'MongoDB Storage')
